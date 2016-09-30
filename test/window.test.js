c10961f174d2d25b8556c8ca509313cebba2478b
Peter Varga <peter@openfin.co>
2016-09-30 13:00:07 +0100

    ~Update test

--- test/window.test.js
+++ test/window.test.js
@@ -15,6 +15,6 @@ describe("Window.", () => {
     it("getBounds()", () => {
         const id = "adapter-test-window"
         return Window.wrap(id, id).getBounds()
-            .should.eventually.have.properties([ "height", "width", "top", "left" ])
+            .should.eventually.have.properties({ height: 500, width: 500 })
     })
 })     
