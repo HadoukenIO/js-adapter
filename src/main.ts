66c70195121b5380397e2a30f0d7ef53281b6b54
Peter Varga <peter@openfin.co>
2016-10-19 13:47:38 +0100

    Promise chaining #1

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,10 @@ import { Identity } from "./identity"
 
 /** Connect to an OpenFin Runtime */
 export function connect(address: string, uuid: string): Promise<OpenFinAPI> {
-    return new Promise((resolve, reject) => {
-        const wire = new Transport()
-        wire.connect(address)
-            .then(() => wire.authenticate(new Identity(uuid))
-                .then(token => resolve(new OpenFinAPI(wire, token)))
-                .catch(reject)
-            )
-            .catch(reject)
-    })
+    const wire = new Transport()
+    return wire.connect(address)
+        .then(() => wire.authenticate(new Identity(uuid)))
+        .then(token => new OpenFinAPI(wire, token))
 }
 
 export { Identity } from "./identity"
