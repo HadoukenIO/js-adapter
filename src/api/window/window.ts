f06ef34897e6a795ab3e1f3a63eaed565a05125a
Peter <vp2177@gmail.com>
2016-10-25 11:17:18 +0100

    Window.moveBy() + "bounds-changed" event, fully typed

--- src/api/window/window.ts
+++ src/api/window/window.ts
@@ -1,6 +1,7 @@
 import { Base, Base_with_Identity } from "../base"
 import { Identity } from "../../identity"
 import Bounds from "./bounds"
+import BoundsChangedReply from "./bounds-changed"
 
 // The window.Window name is taken
 export default class _WindowModule extends Base {
@@ -14,16 +15,20 @@ export class _Window extends Base_with_Identity {
         super(wire)
     }
     getBounds(): Promise<Bounds> {
-        return this.wire.sendAction("get-window-bounds", this.identity.toWireObject())
+        return this.wire.sendAction("get-window-bounds", this.identity)
             .then(({ payload }) => payload.data as Bounds)
     }
     focus(): Promise<void> {
-        return this.wire.sendAction("focus-window", this.identity.toWireObject())
+        return this.wire.sendAction("focus-window", this.identity)
     }
     blur(): Promise<void> {
-        return this.wire.sendAction("blur-window", this.identity.toWireObject())
+        return this.wire.sendAction("blur-window", this.identity)
+    }
+    moveBy(deltaLeft: number, deltaTop: number): Promise<void> {
+        return this.wire.sendAction("move-window-by", this.identity.mergeWith({ deltaLeft, deltaTop }))
     }
 }
 export interface _Window {
     addEventListener(type: "focused", listener: Function) 
+    addEventListener(type: "bounds-changed", listener: (data: BoundsChangedReply) => void) 
 }
