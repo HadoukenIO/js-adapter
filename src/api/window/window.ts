84555c74a243fae4c1a1ffa8a7053344e7e756c2
Ricardo De Peña <ricardo@isnull.co>
2016-12-13 15:41:48 -0500

    RUN-2324 Feature/window_api (#17)
    * window first pass
    
    * window first pass
    
    * added window api tests

--- src/api/window/window.ts
+++ src/api/window/window.ts
@@ -3,6 +3,7 @@ import { WindowIdentity } from "../../identity";
 import Bounds from "./bounds";
 import BoundsChangedReply from "./bounds-changed";
 import Animation from "./animation";
+import { Application } from "../application/application";
 
 export default class _WindowModule extends Bare {
     wrap(identity: WindowIdentity): _Window {
@@ -16,6 +17,16 @@ export class _Window extends Base {
         super(wire);
     }
 
+    private windowListFromNameList(nameList: Array<string>): Array<_Window> {
+        let windowList:Array<_Window> = [];
+
+        for (let i = 0; i < nameList.length; i++) {
+            windowList.push(new _Window(this.wire,
+                                        new WindowIdentity(this.identity.uuid as string, nameList[i])));
+        }
+        return windowList;
+    }
+    
     getBounds(): Promise<Bounds> {
         return this.wire.sendAction("get-window-bounds", this.identity)
             .then(({ payload }) => payload.data as Bounds);
@@ -24,19 +35,15 @@ export class _Window extends Base {
     focus(): Promise<void> {
         return this.wire.sendAction("focus-window", this.identity);
     }
+    
     blur(): Promise<void> {
         return this.wire.sendAction("blur-window", this.identity);
     }
+    
     bringToFront(): Promise<void> {
         return this.wire.sendAction("bring-window-to-front", this.identity);
     }
 
-    moveBy(deltaLeft: number, deltaTop: number): Promise<void> {
-        return this.wire.sendAction("move-window-by", this.identity.mergeWith({ deltaLeft, deltaTop }));
-    }
-    moveTo(left: number, top: number): Promise<void> {
-        return this.wire.sendAction("move-window", this.identity.mergeWith({ left, top }));
-    }
     animationBuilder(interrupt = false): Animation {
         return new Animation(this.wire, this.identity, interrupt);
     }
@@ -44,10 +51,7 @@ export class _Window extends Base {
     hide(): Promise<void> {
         return this.wire.sendAction("hide-window", this.identity);
     }
-    show(force = false): Promise<void> {
-        return this.wire.sendAction("show-window", this.identity.mergeWith({ force }));
-    }
-
+    
     close(force = false): Promise<void> {
         return this.wire.sendAction("close-window", this.identity.mergeWith({ force }))
             .then(() => Object.setPrototypeOf(this, null));
@@ -57,6 +61,152 @@ export class _Window extends Base {
         return this.wire.sendAction("get-window-native-id", this.identity)
             .then(({ payload }) => payload.data);
     }
+
+    disableFrame(): Promise<void> {
+        return this.wire.sendAction("disable-window-frame", this.identity);
+    }
+
+    enableFrame(): Promise<void> {
+        return this.wire.sendAction("enable-window-frame", this.identity);
+    }
+
+    executeJavaScript(code: string): Promise<void> {
+        return this.wire.sendAction("execute-javascript-in-window", this.identity.mergeWith({ code }));
+    }
+
+    flash(): Promise<void> {
+        return this.wire.sendAction("flash-window", this.identity);
+    }
+
+    stopFlashing(): Promise<void> {
+        return this.wire.sendAction("stop-flash-window", this.identity);
+    }
+
+    getGroup(): Promise<Array<_Window>> {
+        return this.wire.sendAction("get-window-group", this.identity).then(({ payload }) => {
+            let winGroups:Array<Array<_Window>> = [] as Array<Array<_Window>>;
+            for (let i = 0; i < payload.data.length; i++) {
+                winGroups[i] = this.windowListFromNameList(payload.data[i]);
+            }
+
+            return winGroups;
+        });
+    }
+
+    getOptions(): Promise<any> {
+        return this.wire.sendAction("get-window-options", this.identity).then(({ payload }) => payload.data);
+    }
+
+    getParentApplication(): Promise<Application> {
+        return Promise.resolve(new Application(this.wire, this.identity));
+    }
+
+    getParentWindow(): Promise<_Window> {
+        return Promise.resolve(new Application(this.wire, this.identity)).then(app => app.getWindow());
+    }
+
+    getSnapshot(): Promise<string> {
+        return this.wire.sendAction("get-window-snapshot", this.identity).then(({ payload }) => payload.data);
+    }
+
+    getState(): Promise<string> {
+        return this.wire.sendAction("get-window-state", this.identity).then(({ payload }) => payload.data);
+    }
+
+    isShowing(): Promise<boolean> {
+        return this.wire.sendAction("is-window-showing", this.identity).then(({ payload }) => payload.data);
+    }
+
+    joinGroup(target: _Window): Promise<void> {
+        return this.wire.sendAction("join-window-group", this.identity.mergeWith({
+            groupingUuid: target.identity.uuid,
+            groupingWindowName: target.identity.name
+        })).then(({ payload }) => payload.data);
+    }
+
+    leaveGroup(): Promise<void> {
+        return this.wire.sendAction("leave-window-group", this.identity);
+    }
+
+    maximize(): Promise<void> {
+        return this.wire.sendAction("maximize-window", this.identity);
+    }
+
+    mergeGroups(target: _Window): Promise<void> {
+        return this.wire.sendAction("join-window-group", this.identity.mergeWith({
+            groupingUuid: target.identity.uuid,
+            groupingWindowName: target.identity.name
+        })).then(({ payload }) => payload.data);
+    }
+
+    minimize(): Promise<void> {
+        return this.wire.sendAction("minimize-window", this.identity);
+    }
+
+    moveBy(deltaLeft: number, deltaTop: number): Promise<void> {
+        return this.wire.sendAction("move-window-by", this.identity.mergeWith({ deltaLeft, deltaTop }));
+    }
+
+    moveTo(left: number, top: number): Promise<void> {
+        return this.wire.sendAction("move-window", this.identity.mergeWith({ left, top }));
+    }
+
+    resizeBy(deltaWidth: number, deltaHeight: number, anchor: string): Promise<void> {
+        return this.wire.sendAction("resize-window-by", this.identity.mergeWith({
+            deltaWidth: Math.floor(deltaWidth),
+            deltaHeight: Math.floor(deltaHeight),
+            anchor
+        }));
+    }
+
+    resizeTo(width: number, height: number, anchor: string): Promise<void> {
+        return this.wire.sendAction("resize-window", this.identity.mergeWith({
+            width: Math.floor(width),
+            height: Math.floor(height),
+            anchor
+        }));
+    }
+
+    restore(): Promise<void> {
+        return this.wire.sendAction("restore-window", this.identity);
+    }
+
+    setAsForeground(): Promise<void> {
+        return this.wire.sendAction("set-foreground-window", this.identity);
+    }
+
+    setBounds(bounds: Bounds): Promise<void> {
+        return this.wire.sendAction("set-window-bounds", this.identity.mergeWith(bounds));
+    }
+
+    show(force = false): Promise<void> {
+        return this.wire.sendAction("show-window", this.identity.mergeWith({ force }));
+    }
+
+    showAt(left: number, top: number, force = false): Promise<void> {
+        return this.wire.sendAction("show-at-window", this.identity.mergeWith({
+            force,
+            left: Math.floor(left),
+            top: Math.floor(top)
+        }));
+    }
+
+    updateOptions(options: any): Promise<void> {
+        return this.wire.sendAction("show-window", this.identity.mergeWith({ options }));
+    }
+
+    authenticate(userName: string, password: string): Promise<void> {
+        return this.wire.sendAction("window-authenticate", this.identity.mergeWith({ userName, password }));
+    }
+
+    getZoomLevel(): Promise<number> {
+        return this.wire.sendAction("get-zoom-level", this.identity).then(({ payload }) => payload.data);
+    }
+
+    setZoomLevel(level: number): Promise<void> {
+        return this.wire.sendAction("set-zoom-level", this.identity.mergeWith({ level })).then(( { payload }) => payload.data);
+    }
+    
 }
 export interface _Window {
     addEventListener(type: "focused", listener: Function);
--- test/window.test.js
+++ test/window.test.js
@@ -1,33 +1,307 @@
 const { describe, it } = require("mocha"),
     should = require("should"),
-    connect = require("./connect"),
-    { Identity } = require("../."), 
-    id = "adapter-test-window";
+      connect = require("./connect"),
+      { Identity, WindowIdentity } = require("../out/identity.js"),
+      appConfig = require("./app.json");
+
 
 describe("Window.", () => {
-    let fin, imag, real;
+    let fin,
+        testApp,
+        testWindow;
 
+    const appUuid = appConfig.startup_app.uuid,
+          appConfigTemplate = {
+            "name": "adapter-test-app-win",
+            "url": "about:blank",
+            "uuid": "adapter-test-app-win",
+            "autoShow": true
+    };
+    
+    
     before(() => {
-        return connect().then(a => {
-            fin = a;
-            imag = fin.Window.wrap(new Identity("my-uuid-123", "my-window"));
-            real = fin.Window.wrap(new Identity(id, id));
-        })
-    })
+        return connect().then(a => fin = a);
+    });
 
-    describe("wrap(non-existent).", () => {
-        it("^", () => {
-            imag.should.be.Object();
-        })
-        it("getNativeId()", () => {
-            return imag.getNativeId()
-                .should.be.rejected();
-        })
-    })
-    describe("wrap().", () => {
-        it("getBounds()", () => {
-            return real.getBounds()
-                .should.eventually.have.property("height").a.Number();
-        })
-    })
-})     
+    beforeEach(() => {
+        return fin.Application.create(appConfigTemplate).then(a => {
+            testApp = a;
+            return testApp.run().then(() =>  testApp.getWindow().then(w =>  testWindow = w));
+        });
+    });
+
+   afterEach(() => testApp.close());
+    
+    describe("getBounds()", () => {
+        
+        const bounds = {
+            height: 400,
+            width: 400,
+            top: 10,
+            left: 10
+        }; 
+        
+        it("Fulfilled", () => testWindow.setBounds(bounds).then(testWindow.getBounds().should.eventually.have.property("height").a.Number()));
+    });
+
+    describe("focus()", () => {
+
+        it("Fulfilled", () => testWindow.focus().should.be.fulfilled());
+    });
+
+    describe("blur()", () => {
+
+        it("Fulfilled", () => testWindow.blur().should.be.fulfilled());
+    });
+
+    describe("bringToFront()", () => {
+
+        it("Fulfilled", () => testWindow.bringToFront().should.be.fulfilled());
+    });
+
+    describe("hide()", () => {
+        
+        it("Fulfilled", () => testWindow.hide().should.be.fulfilled());
+    });
+
+    describe("close()", () => {
+
+        const appToCloseConfig = {
+            "name": "adapter-test-app-to-close",
+            "url": "about:blank",
+            "uuid": "adapter-test-app-to-close",
+            "autoShow": true
+        };
+        let appToClose,
+            winToClose;
+        
+        before(() => {
+            return fin.Application.create(appToCloseConfig).then(a => {
+                appToClose = a;
+                return appToClose.run().then(() =>  appToClose.getWindow().then(w =>  winToClose = w));
+            });
+        });
+        
+        it("Fulfilled", () => winToClose.close().then(() => appToClose.isRunning().should.be.fulfilledWith(false)));
+    });
+
+    describe("getNativeId()", () => {
+
+        it("Fulfilled", () => testWindow.getNativeId().should.eventually.be.String());
+    });
+
+    describe("disableFrame()", () => {
+
+        it("Fulfilled", () => testWindow.disableFrame().should.be.fulfilled());
+    });
+
+    describe("enableFrame()", () => {
+
+        it("Fulfilled", () => testWindow.enableFrame().should.be.fulfilled());
+    });
+
+    describe("executeJavaScript()", () => {
+        
+        const scriptToExecute = "console.log('hello world')";
+        
+        it("Fulfilled", () => testWindow.executeJavaScript(scriptToExecute).should.be.fulfilled());
+    });
+
+    describe("flash()", () => {
+        
+        it("Fulfilled", () => testWindow.flash().should.be.fulfilled());
+    });
+
+    describe("getGroup()", () => {
+
+        it("Fulfilled", () => testWindow.getGroup().should.be.fulfilledWith([]));
+    });
+
+    describe("getOptions()", () => {
+
+        it("Fulfilled", () => testWindow.getOptions().should.be.fulfilled());
+    });
+
+    describe("getParentApplication()", () => {
+
+        it("Fulfilled", () => testWindow.getParentApplication().should.eventually
+           .have.propertyByPath("identity", "uuid").equal(appConfigTemplate.uuid));
+    });
+
+    describe("getParentWindow()", () => {
+
+        it("Fulfilled", () => testWindow.getParentWindow().should.eventually
+           .have.propertyByPath("identity", "name").equal(appConfigTemplate.uuid));
+    });
+
+    describe("getSnapshot()", () => {
+
+        it("Fulfilled", () => testWindow.getSnapshot().should.be.fulfilled());
+    });
+
+    describe("getState()", () => {
+
+        it("Fulfilled", () => testWindow.getState().should.eventually.be.String());
+    });
+
+    describe("isShowing()", () => {
+
+        it("Fulfilled", () => testWindow.isShowing().should.eventually.be.Boolean());
+    });
+
+    //TODO: feature needs testing.
+    // describe("joinGroup()", () => {
+
+    //     it("Fulfilled", () => testWindow.joinGroup().should.eventually.be.Boolean());
+    // });
+
+    //TODO: feature needs testing.
+    // describe("leaveGroup()", () => {
+
+    //     it("Fulfilled", () => testWindow.leaveGroup().should.eventually.be.Boolean());
+    // });
+
+    describe("maximize()", () => {
+        
+        it("Fulfilled", () => testWindow.maximize().should.be.fulfilled());
+    });
+
+    //TODO: feature needs testing.
+    // describe("mergeGroups()", () => {
+
+    //     it("Fulfilled", () => testWindow.mergeGroups().should.eventually.be.Boolean());
+    // });
+
+    describe("minimize()", () => {
+        
+        it("Fulfilled", () => testWindow.minimize().then(() => testWindow.getState()).should.be.fulfilledWith('minimized'));
+    });
+
+    describe("moveBy()", () => {
+        
+        it("Fulfilled", () => {
+            
+            return testWindow.getBounds().then(bounds => {
+                bounds.top++;
+                bounds.left++;
+                bounds.bottom++;
+                bounds.right++;
+                
+                return testWindow.moveBy(1, 1).then(() => testWindow.getBounds().should.be.fulfilledWith(bounds));
+            });
+            
+        });
+    });
+
+    describe("moveTo()", () => {
+
+        it("Fulfilled", () => {
+            
+            return testWindow.getBounds().then(bounds => {
+                bounds.top = 10;
+                bounds.left = 10;
+                
+                return testWindow.moveTo(10, 10).then(() =>  testWindow.getBounds().should.be.fulfilledWith(bounds));
+            });        
+        });
+    });
+
+    describe("resizeBy()", () => {
+
+        it("Fulfilled", () => {
+            let bounds;
+            
+            return testWindow.getBounds().then(b => {
+                bounds = b;
+                bounds.bottom += 10;
+                bounds.right += 10;
+                bounds.height += 10;
+                bounds.width += 10;
+                
+                return testWindow.resizeBy(10, 10, "top-left").then(() => testWindow.getBounds().should.be.fulfilledWith(bounds));
+            });
+        });
+    });
+
+    describe("resizeTo()", () => {
+        
+        it("Fulfilled", () => {
+            let bounds;
+            
+            return testWindow.getBounds().then(b => {
+                bounds = b;
+                bounds.bottom = 20;
+                bounds.right = 20;
+                bounds.height = 10;
+                bounds.width = 10;
+                
+                return testWindow.resizeTo(10, 10, "top-left").then(() => testWindow.getBounds().should.be.fulfilledWith(bounds));
+            });
+        });
+    });
+
+    describe("setAsForeground()", () => {
+        
+        it("Fulfilled", () => testWindow.setAsForeground().should.be.fulfilled());
+    });
+
+    describe("setBounds()", () => {
+
+        const bounds = {
+            height: 400,
+            width: 400,
+            top: 10,
+            left: 10,
+            bottom: 410,
+            right: 410
+        };
+        
+        it("Fulfilled", () => testWindow.setBounds(bounds).then(() => testWindow.getBounds().should.be.fulfilledWith(bounds)));
+    });
+
+    describe("show()", () => {
+        
+        it("Fulfilled", () => testWindow.show().should.be.fulfilled());
+    });
+
+    describe("showAt()", () => {
+
+        it("Fulfilled", () => {
+
+            return testWindow.getBounds().then(bounds => {
+                bounds.top = 10;
+                bounds.left = 10;
+                
+                return testWindow.showAt(10, 10).then(() =>  testWindow.getBounds().should.be.fulfilledWith(bounds));
+            });
+        });
+    });
+
+    describe("updateOptions()", () => {
+
+        const updatedOptions = {
+            height: 100
+        };
+        
+        it("Fulfilled", () => testWindow.updateOptions(updatedOptions).should.be.fulfilled());
+    });
+
+    //TODO: feature needs testing.
+    // describe("authenticate()", () => {
+        
+    //     it("Fulfilled", () => testWindow.authenticate(10, 10).should.be.fulfilled());
+    // });
+
+
+    describe("getZoomLevel()", () => {
+        
+        it("Fulfilled", () => testWindow.getZoomLevel().should.be.fulfilledWith(0));
+    });
+
+    describe("setZoomLevel()", () => {
+
+        const zoomLevel = 1;
+        
+        it("Fulfilled", () => testWindow.setZoomLevel(zoomLevel).then(() => testWindow.getZoomLevel()).should.be.fulfilledWith(zoomLevel));
+    });
+});
