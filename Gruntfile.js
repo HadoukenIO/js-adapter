01cf1ea1820dfe21049e4c18926b8a0b5dd08870
andreGarvin <andregarvin718@gmail.com>
2017-08-01 16:19:35 -0400

    changed the commit message for the grunt task for the publish-docs task

--- Gruntfile.js
+++ Gruntfile.js
@@ -86,14 +86,18 @@ module.exports = function(grunt) {
           ps.lookup({
               command: 'openfin.exe'
           }, (err, processList) => {
-              if (err) throw new Error(err);
+              if (err) {
+                  throw new Error(err);
+              }
               processList.forEach(i=> ps.kill(i.pid));
           });
     });
 
     grunt.registerTask('publish-docs', () => {
-        exec('cd docs && git commit -am "committed new update for node-adapter docuemtation github pages" && git push origin master', function(err, stdout, stderr) {
-             if (err) return console.log(err);
+        exec(`cd docs && git commit -am "committed new update veriosn ${ verison } for node-adapter documentation." && git push origin master`, function(err, stdout, stderr) {
+             if (err) {
+                return console.log(err);
+             }
              console.log('published new documentationfor node-adapter.');
         });
     });
