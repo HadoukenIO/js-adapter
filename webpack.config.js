987176fa56be507ae8222fe56615ad61113ed6a8
andreGarvin <andregarvin718@gmail.com>
2017-10-31 10:07:38 -0400

    webpack set up for the bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,19 @@
 const { resolve, dirname } = require('path')
 
+const nodeExternals = require('webpack-node-externals')
+
 const entryPath = resolve(`${__dirname}/src`)
 const outPath = resolve(`${ __dirname }/dist`)
 module.exports = {
-    entry: resolve(entryPath, '/main.ts'),
+    entry: resolve(entryPath, 'main.ts'),
+    target: 'node',
+    externals: [ nodeExternals() ],
     output: {
         path: outPath,
         filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.ts']
     },
     module: {
         loaders: [
