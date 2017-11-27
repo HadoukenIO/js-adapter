bfdeb9b1680138148db4a5432641281b513c3e15
licui <li@openfin.co>
2017-11-27 11:18:32 -0500

    update the test

--- test/external-application.test.ts
+++ test/external-application.test.ts
@@ -7,12 +7,15 @@ describe('ExternalApplication.', () => {
     before(() => conn().then((a: Fin) => fin = a));
 
     describe('getInfo()', () => {
-
-        it('Should contain parent information', () => fin.ExternalApplication.wrap('notepad-uuid')
-            .then(extApp => {
-                extApp.getInfo().then(info => {
-                    // todo: need to get parent uuid and name info
-                    assert(info.parent === null);
+        it('Fulfilled', () => fin.System.getAllExternalApplications().
+            then(apps => {
+                const uuids = apps.map((app: any) => app.uuid);
+                // get the uuid of first exteranl application to wrap if it exists
+                fin.ExternalApplication.wrap(uuids[0] || 'notepad-uuid')
+                .then(extApp => {
+                    extApp.getInfo().then(info => {
+                        assert(typeof(info) === 'object');
+                    });
                 });
         }));
     });
