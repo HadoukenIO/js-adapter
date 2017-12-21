19ad37a3919ddbb0cc1cf39094d381a5615fa698
Pierre Baize <pierre@Pierres-MacBook-Pro.local>
2017-12-21 12:24:30 -0500

    undid auto format gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,53 +1,53 @@
-const path = require("path");
-const testAppConfig = path.resolve("test", "app.json");
-const liveServer = require("live-server");
-const ps = require("ps-node");
-const exec = require("child_process").exec;
-const rimraf = require("rimraf");
-const webpack = require("webpack");
+const path = require('path');
+const testAppConfig = path.resolve('test', 'app.json');
+const liveServer = require('live-server');
+const ps = require('ps-node');
+const exec = require('child_process').exec;
+const rimraf = require('rimraf');
+const webpack = require('webpack');
 
-const outDir = path.resolve("out");
+const outDir = path.resolve('out');
 const webpackConfig = {
-    entry: "./out/src/of-main.js",
+    entry: './out/src/of-main.js',
     output: {
-        filename: "./out/js-adapter.js"
+        filename: './out/js-adapter.js'
     }
 };
 const serverParams = {
-    root: path.resolve("html"),
+    root: path.resolve('html'),
     open: false,
     logLevel: 0,
     port: 8689
 };
 
 module.exports = function(grunt) {
-    const version = grunt.option("ver");
-    const remote = grunt.option("remote");
-    const uuid = "testapp";
-    const args = "--enable-multi-runtime --debug=5858";
+    const version = grunt.option('ver');
+    const remote = grunt.option('remote');
+    const uuid = 'testapp';
+    const args = '--enable-multi-runtime --debug=5858';
     process.env.OF_VER = version;
     process.env.TEST_SERVER_PORT = serverParams.port;
 
     grunt.initConfig({
         shell: {
             build: {
-                command: "tsc",
+                command: 'tsc',
                 preferLocal: true
             }
         },
         tslint: {
             default: {
-                src: ["src/**/*.ts", "repl/*.ts", "test/*.ts"]
+                src: ['src/**/*.ts', 'repl/*.ts', 'test/*.ts']
             },
             options: {
-                configuration: grunt.file.readJSON("tslint.json"),
-                rulesDirectory: "node_modules/tslint-microsoft-contrib",
+                configuration: grunt.file.readJSON('tslint.json'),
+                rulesDirectory: 'node_modules/tslint-microsoft-contrib',
                 force: false
             }
         },
         mochaTest: {
             default: {
-                src: "out/test/**/*.js"
+                src: 'out/test/**/*.js'
             }
         },
         openfin: {
@@ -80,53 +80,53 @@ module.exports = function(grunt) {
         },
         copy: {
             resources: {
-                files: [{ src: "resources/**", dest: "out/" }]
+                files: [{ src: 'resources/**', dest: 'out/' }]
             }
         }
     });
 
-    grunt.registerTask("check-version", function() {
+    grunt.registerTask('check-version', function() {
         if (!version) {
             grunt.fail.fatal(
-                "No version given, please provide a target version"
+                'No version given, please provide a target version'
             );
         }
     });
 
-    grunt.registerTask("clean", function() {
+    grunt.registerTask('clean', function() {
         rimraf.sync(outDir);
-        grunt.log.ok("out directory deleted");
+        grunt.log.ok('out directory deleted');
     });
 
-    grunt.registerTask("start-server", function() {
+    grunt.registerTask('start-server', function() {
         const done = this.async();
-        liveServer.start(serverParams).on("listening", done);
+        liveServer.start(serverParams).on('listening', done);
     });
 
-    grunt.registerTask("start-repl", function() {
-        const finRepl = require(path.join(outDir, "repl", "index.js"));
+    grunt.registerTask('start-repl', function() {
+        const finRepl = require(path.join(outDir, 'repl', 'index.js'));
         const done = this.async();
         finRepl.startRepl();
     });
 
-    grunt.registerTask("webpack", function() {
+    grunt.registerTask('webpack', function() {
         const done = this.async();
         webpack(webpackConfig, (err, stats) => {
             if (err || stats.hasErrors()) {
-                const error = err ? err.message : "webpack error";
+                const error = err ? err.message : 'webpack error';
                 grunt.log.error(error);
                 done(err);
             } else {
-                grunt.log.ok("webpack task done");
+                grunt.log.ok('webpack task done');
                 done();
             }
         });
     });
 
-    grunt.registerTask("kill-processes", function() {
+    grunt.registerTask('kill-processes', function() {
         ps.lookup(
             {
-                command: "openfin.exe"
+                command: 'openfin.exe'
             },
             (err, processList) => {
                 if (err) {
@@ -137,7 +137,7 @@ module.exports = function(grunt) {
         );
     });
 
-    grunt.registerTask("publish-docs", () => {
+    grunt.registerTask('publish-docs', () => {
         exec(
             `cd docs && git commit -am "committed new update for node-adapter documentation." && git push ${remote} master`,
             function(err, stdout, stderr) {
@@ -145,39 +145,39 @@ module.exports = function(grunt) {
                     grunt.log.error(err);
                 } else {
                     grunt.log.ok(
-                        "published new documentation for node-adapter."
+                        'published new documentation for node-adapter.'
                     );
                 }
             }
         );
     });
 
-    grunt.loadNpmTasks("grunt-shell");
-    grunt.loadNpmTasks("grunt-tslint");
-    grunt.loadNpmTasks("grunt-mocha-test");
-    grunt.loadNpmTasks("grunt-openfin");
-    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-tslint');
+    grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-openfin');
+    grunt.loadNpmTasks('grunt-contrib-copy');
 
-    grunt.registerTask("lint", ["tslint"]);
-    grunt.registerTask("build", [
-        "clean",
-        "shell",
-        "webpack",
-        "copy:resources"
+    grunt.registerTask('lint', ['tslint']);
+    grunt.registerTask('build', [
+        'clean',
+        'shell',
+        'webpack',
+        'copy:resources'
     ]);
-    grunt.registerTask("default", ["lint", "build"]);
-    grunt.registerTask("test", [
-        "check-version",
-        "default",
-        "start-server",
-        "mochaTest",
-        "kill-processes"
+    grunt.registerTask('default', ['lint', 'build']);
+    grunt.registerTask('test', [
+        'check-version',
+        'default',
+        'start-server',
+        'mochaTest',
+        'kill-processes'
     ]);
-    grunt.registerTask("repl", [
-        "check-version",
-        "default",
-        "start-server",
-        "openfin",
-        "start-repl"
+    grunt.registerTask('repl', [
+        'check-version',
+        'default',
+        'start-server',
+        'openfin',
+        'start-repl'
     ]);
 };
