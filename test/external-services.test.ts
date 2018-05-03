12a04cd639b9a4967e901db008ed211ecb21f695
Thomas <tom@openfin.co>
2018-05-03 10:30:31 -0400

    hiold

---
+++ test/external-services.test.ts
@@ -0,0 +1,39 @@
+import * as assert from 'assert';
+import { conn } from './connect';
+import { Fin } from '../src/main';
+import * as sinon from 'sinon';
+import { cleanOpenRuntimes } from './multi-runtime-utils';
+
+describe ('Event Emitter Methods', () => {
+    let fin: Fin;
+    let app: any;
+    let win: any;
+    const appConfigTemplate = {
+        name: 'adapter-test-app',
+        url: 'about:blank',
+        uuid: 'adapter-test-app',
+        autoShow: true,
+        saveWindowState: false,
+        accelerator: {
+            devtools: true
+        }
+    };
+
+    before(() => {
+        return conn().then(async a => {
+            fin = a;
+            await cleanOpenRuntimes();
+        });
+    });
+
+    beforeEach(async () => {
+        app = await fin.Application.create(appConfigTemplate);
+        await app.run();
+        win = await app.getWindow();
+    });
+
+    afterEach(async() => {
+        await app.close();
+    });
+
+    describe('External Services')
