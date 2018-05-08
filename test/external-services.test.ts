58fddad67c78cc10fa96a66b0ab454ef1dcecf0d
Thomas <tom@openfin.co>
2018-05-08 09:47:46 -0400

    tests for external services re RUN-3898

--- test/external-services.test.ts
+++ test/external-services.test.ts
@@ -1,45 +1,110 @@
 import * as assert from 'assert';
 import { conn } from './connect';
-import { Fin } from '../src/main';
-// import * as sinon from 'sinon';
-import { cleanOpenRuntimes } from './multi-runtime-utils';
+import { Fin, launch } from '../src/main';
+import * as path from 'path';
+import { cleanOpenRuntimes/* , killByPort */ } from './multi-runtime-utils';
+import { delayPromise } from './delay-promise';
+import * as sinon from 'sinon';
+import * as fs from 'fs';
 
-describe ('tommy', () => {
+describe ('External Services', () => {
     let fin: Fin;
-    let app: any;
-    let win: any;
-    const appConfigTemplate = {
-        name: 'adapter-test-app',
-        url: 'about:blank',
-        uuid: 'adapter-test-app',
-        autoShow: true,
-        saveWindowState: false,
-        accelerator: {
-            devtools: true
-        }
-    };
-
-    before(() => {
-        return conn().then(async a => {
-            fin = a;
-            await cleanOpenRuntimes();
-        });
-    });
+    let appConfig: any;
 
     beforeEach(async () => {
-        app = await fin.Application.create(appConfigTemplate);
-        await app.run();
-        win = await app.getWindow();
+        await cleanOpenRuntimes();
+        appConfig = JSON.parse(fs.readFileSync(path.resolve('test/app.json')).toString());
+        fin = await conn();
     });
 
-    afterEach(async() => {
-        await app.close();
+    // tslint:disable-next-line
+    describe('External Provider', function () {
+
+        it('Should be able to register as Provider', function(done: any) {
+            // tslint:disable-next-line no-invalid-this
+            this.timeout(8000);
+
+            const url = appConfig.startup_app.url;
+            const newUrl = url.slice(0, url.lastIndexOf('/')) + '/client.html';
+
+            const clientConfig : any = {
+                ...appConfig,
+                'startup_app': {
+                    'name': 'Services',
+                    'description': 'services test app',
+                    'url': newUrl,
+                    'uuid': 'service-client-test',
+                    'autoShow': true,
+                    'saveWindowState': false,
+                    'nonPersistent': true,
+                    'experimental': {
+                        'v2Api': true
+                    }
+                }
+            };
+
+            fs.writeFileSync(path.resolve('test/client.json'), JSON.stringify(clientConfig));
+
+            async function test () {
+                const spy = sinon.spy();
+                const provider = await fin.Service.register();
+                provider.register('test', () => {
+                    spy();
+                    return 'return-test';
+                });
+                provider.onConnection(c => {
+                    spy();
+                });
+                await launch({manifestUrl: path.resolve('test', 'client.json')});
+                await fin.InterApplicationBus.subscribe({uuid: 'service-client-test'}, 'return', (msg: any) => {
+                    assert(spy.calledTwice && msg === 'return-test', 'Did not get IAB from dispatch');
+                    done();
+                });
+                await delayPromise(1000);
+                await fin.InterApplicationBus.publish('start', 'hi');
+            }
+            test();
+        });
+
     });
 
-    describe('External Services',  () => {
-        it('Should be able to connect as Provider', async () => {
-            const provider = await fin.Service.register();
-            assert(provider);
+    // tslint:disable-next-line
+    describe('External Client', function () {
+
+        it('Should be able to connect as Client', function(done: any) {
+            // tslint:disable-next-line no-invalid-this
+            this.timeout(8000);
+
+            const url = appConfig.startup_app.url;
+            const newUrl = url.slice(0, url.lastIndexOf('/')) + '/service.html';
+
+            const serviceConfig : any = {
+                ...appConfig,
+                'startup_app': {
+                    'name': 'Service Provider',
+                    'description': 'Service Provider test app',
+                    'url': newUrl,
+                    'uuid': 'service-provider-test',
+                    'autoShow': true,
+                    'saveWindowState': false,
+                    'nonPersistent': true,
+                    'experimental': {
+                        'v2Api': true
+                    }
+                }
+            };
+
+            fs.writeFileSync(path.resolve('test/service.json'), JSON.stringify(serviceConfig));
+
+            async function test() {
+                await launch({manifestUrl: path.resolve('test', 'service.json')});
+                const client = await fin.Service.connect({uuid: 'service-provider-test'});
+                client.dispatch('test').then(res => {
+                    assert(res === 'return-test');
+                    done();
+                });
+            }
+            test();
         });
     });
 });
