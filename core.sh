bd58895b53a7834dc37b6f048cde17560a882bfa
Aziz Yokubjonov <aziz.yokubjonov@gmail.com>
2018-07-24 13:54:24 -0400

    add js-adapter testing with core

---
+++ core.sh
@@ -0,0 +1,25 @@
+# CORE_PATH - path of the core dir
+# TARBALL_NAME - tarball file name
+# HODUKEN_DIR - js-adapter folder path
+
+npm pack
+TARBALL_NAME=$(ls hadouken-js-adapter-* -t | head -1)
+ADAPTER_DIR=$(pwd)
+
+if [ ! -z "$CORE_PATH" ]; then
+    cd $CORE_PATH
+else
+    cd ..
+    git clone https://github.com/HadoukenIO/core.git
+    cd core
+    CORE_PATH=$(pwd)
+fi
+
+npm install
+cp $ADAPTER_DIR/$TARBALL_NAME ./
+npm install $TARBALL_NAME
+npm install openfin-sign
+npm run build
+cd out
+echo Finished! .asar was created in $(pwd)
+$SHELL
