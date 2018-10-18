e22ae5ec88ebc5694343f3a3d9aef674c2f5b6bb
licui <li@openfin.co>
2018-10-18 16:23:45 -0400

    RUN-4713 delay some time in setZoomLevel test to make sure it's actually set

--- test/application.test.ts
+++ test/application.test.ts
@@ -1,6 +1,7 @@
 import { conn } from './connect';
 import { Fin, Application, Window, connect as rawConnect } from '../src/main';
 import { cleanOpenRuntimes } from './multi-runtime-utils';
+import { delayPromise } from './delay-promise';
 import * as assert from 'assert';
 import * as path from 'path';
 
@@ -294,6 +295,7 @@ describe('Application.', function() {
         it('Fulfilled', async () => {
 
             await testApp.setZoomLevel(zoomLevel);
+            await delayPromise(200); // delay some time to make sure zoom level is actually set
             const newZoomLevel = await testApp.getZoomLevel();
             assert(newZoomLevel === zoomLevel);
         });
