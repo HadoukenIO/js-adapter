197a3bb980978bb73c7f5be20a1d3084749a13c1
Luis Espinola <lespinola@gmail.com>
2019-01-29 17:17:50 -0500

    recommit

--- src/launcher/win-launch.ts
+++ src/launcher/win-launch.ts
@@ -1,6 +1,7 @@
 import * as path from 'path';
 import { ChildProcess, spawn } from 'child_process';
 import { ConfigWithRuntime } from '../transport/wire';
+import { exists } from './util';
 
 function launchRVM(config: ConfigWithRuntime, manifestLocation: string, namedPipeName: string, rvm: string): ChildProcess {
     const runtimeArgs = `--runtime-arguments=--runtime-information-channel-v6=${namedPipeName}`;
@@ -18,8 +19,33 @@ function launchRVM(config: ConfigWithRuntime, manifestLocation: string, namedPip
     return spawn(rvm, rvmArgs, { stdio: ['pipe', 'ignore', 'pipe'] });
 }
 
+async function checkRvmPath() {
+    let rvmPath: string = path.resolve(process.env.LOCALAPPDATA, 'OpenFin', 'OpenFinRVM.exe');
+
+    if (! await exists(rvmPath)) {
+        rvmPath = path.join(__dirname, '..', '..', 'resources', 'win', 'OpenFinRVM.exe');
+    }
+
+    return rvmPath;
+}
+
+const checkRVM = makeQueued(checkRvmPath);
+
 // tslint:disable-next-line:max-line-length
 export default async function launch(config: ConfigWithRuntime, manifestLocation: string, namedPipeName: string): Promise<ChildProcess> {
-    const rvmPath = path.join(__dirname, '..', '..', 'resources', 'win', 'OpenFinRVM.exe');
+    const rvmPath = await checkRVM();
     return await launchRVM(config, manifestLocation, namedPipeName, rvmPath);
+}
+
+function makeQueued<R, T extends (...args: any[]) => Promise<R>>(func: T): T {
+    let initial: Promise<R>;
+    return async function (...args: any[]): Promise<R> {
+        const x: Promise<R | void> = initial || Promise.resolve();
+        initial = x
+            .then(() => new Promise<void>((resolve, reject) => setImmediate(() => resolve())))
+            .then(() => func(...args))
+            .catch(() => func(...args));
+        return initial;
+        // tslint:disable-next-line:prefer-type-cast no-function-expression
+    } as T;
 }
