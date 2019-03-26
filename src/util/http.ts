f0736ae16d472f380ac791e222fe9cd71e75c868
Luis Espinola <lespinola@gmail.com>
2019-03-26 07:37:21 -0400

    added proxy support for js adapter

--- src/util/http.ts
+++ src/util/http.ts
@@ -1,11 +1,13 @@
 import { parse } from 'url';
 import { IncomingMessage } from 'http';
+import * as fs from 'fs';
 
 export const fetch = async (url: string): Promise<string> => {
     const proto = (parse(url).protocol.slice(0, -1)) === 'http' ? 'http' : 'https';
     const fetcher = await import(proto);
     return new Promise<string>((resolve, reject) => {
-        const request = fetcher.get(url, (response: IncomingMessage) => {
+        const options = getRequestOptions(url);
+        const request = fetcher.get(options, (response: IncomingMessage) => {
             if (response.statusCode < 200 || response.statusCode > 299) {
                 reject(new Error(`Failed to load url: ${url}, status code:${response.statusCode}`));
             }
@@ -18,6 +20,83 @@ export const fetch = async (url: string): Promise<string> => {
         request.on('error', (err: any) => reject(err));
     });
 };
+
+export const downloadFile = async (url: string, writeLocation: string) => {
+    const proto = (parse(url).protocol.slice(0, -1)) === 'http' ? 'http' : 'https';
+    const fetcher = await import(proto);
+    return new Promise((resolve, reject) => {
+        try {
+            const options = getRequestOptions(url);
+            fetcher.get(options, (response: IncomingMessage) => {
+                const fileSize = parseInt(response.headers['content-length'], 10);
+                let chunkCtr = 0;
+                let progress: number;
+                let output: string;
+
+                response.on('data', (chunk) => {
+                    chunkCtr += chunk.length;
+                    progress = Math.floor(100 * chunkCtr / fileSize);
+                    output = 'Downloading Runtime: ' + progress + '%\r';
+                    if (progress === 100) {
+                        output = 'Downloading Runtime: ' + progress + '%\n';
+                    }
+                    process.stdout.write(output);
+                });
+
+                const file = fs.createWriteStream(writeLocation);
+                response.pipe(file);
+                file.on('finish', () => {
+                    file.close();
+                    resolve();
+                });
+            });
+        } catch (e) {
+            reject(e);
+        }
+    });
+};
+
+export const getProxy = () => {
+    const parsedUrl = new URL(getProxyVar());
+    return {
+        port: parsedUrl.port,
+        host: parsedUrl.hostname,
+        username: parsedUrl.username,
+        password: parsedUrl.password
+    };
+}
+
+const getProxyVar = () => {
+    return process.env.HTTPS_PROXY ||
+        process.env.https_proxy ||
+        process.env.HTTP_PROXY ||
+        process.env.http_proxy;
+}
+
+export const getRequestOptions = (url: string) => {
+    const parsedUrl = new URL(url);
+
+    const options = {
+        host: parsedUrl.hostname,
+        path: parsedUrl.pathname,
+        port: parsedUrl.port,
+        headers: { Host: '' }
+    };
+
+    if (getProxyVar()) {
+        const proxy = getProxy();
+        options.host = proxy.host;
+        options.port = proxy.port;
+        options.path = url;
+        options.headers.Host = parsedUrl.host;
+        if (proxy.username && proxy.password) {
+            const auth = 'Basic ' + Buffer.from(proxy.username + ':' + proxy.password).toString('base64');
+            Object.assign(options.headers, { 'Proxy-Authorization': auth });
+        }
+    }
+
+    return options;
+}
 
 export const fetchJson = async (url: string): Promise<any> => {
     const res = await fetch(url);
