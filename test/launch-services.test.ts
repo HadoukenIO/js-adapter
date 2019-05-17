47aa67e6c934d04f5f5ee0c7ae55fc614a17894e
Ricardo de pena <ricardo@isnull.co>
2019-05-17 14:54:57 -0400

    fixed issue where services test would leave zombie processes

--- test/launch-services.test.ts
+++ test/launch-services.test.ts
@@ -1,8 +1,9 @@
 /* tslint:disable:no-invalid-this no-function-expression insecure-random mocha-no-side-effect-code no-empty */
 import {connect, Fin} from '../src/main';
-import {kill, TEST_TIMEOUT } from './multi-runtime-utils';
+import { TEST_TIMEOUT } from './multi-runtime-utils';
 import * as fs from 'fs';
 import * as path from 'path';
+import * as assert from 'assert';
 
 const servicePath = path.resolve('test', 'service-app.json');
 const appPath = path.resolve('test', 'launch-service-app.json');
@@ -11,10 +12,9 @@ before(async function () {
     const base = JSON.parse(fs.readFileSync(path.resolve('test', 'app.json')).toString());
     const serviceConf = JSON.stringify({
         ...base,
-        startup_app: {uuid: 'TESTSERVICE', name: 'TESTSERVICE'}
+        startup_app: { uuid: 'TESTSERVICE', name: 'TESTSERVICE', 'nonPersistent' : true }
     });
     fs.writeFileSync(servicePath, serviceConf);
-    base.runtime.arguments += ' --security-realm=services-test';
     const conf = JSON.stringify({
         ...base,
         startup_app: null,
@@ -26,9 +26,10 @@ describe('desktop-services', function () {
     this.timeout(TEST_TIMEOUT);
     let fin: Fin;
     it('can launch an app with a service', async function () {
-       fin = await connect({manifestUrl: appPath, uuid: 'service-test-app'});
-    });
-    after(async function () {
-        await kill(fin);
+       fin = await connect({ manifestUrl: appPath, uuid: 'service-test-app', nonPersistent: true });
+       const service = fin.Application.wrapSync({uuid: 'TESTSERVICE' });
+       const isRunning = await service.isRunning();
+       assert(isRunning, 'Expected service to be running');
+
     });
 });
