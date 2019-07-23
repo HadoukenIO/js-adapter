079b02d30b13651cac0437c1720350a5842c60d0
Li Cui <li@openfin.co>
2019-07-23 14:52:30 -0400

    RUN-5240 update customeData type to any (#303)
    * RUN-5240 update customeData type
    
    * RUN-5240 Fix some linting issues found in DefinitelyTyped

--- src/api/application/applicationOption.ts
+++ src/api/application/applicationOption.ts
@@ -22,7 +22,7 @@ export interface ApplicationOption {
     contentNavigation?: object;
     contextMenu?: boolean;
     cornerRounding?: object;
-    customData?: string;
+    customData?: any;
     customRequestHeaders?: Array<CustomRequestHeaders>;
     defaultCentered?: boolean;
     defaultHeight?: number;
