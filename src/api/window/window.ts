336b755762cbee562a12892498a604bdefe05878
Aziz Yokubjonov <aziz.yokubjonov@gmail.com>
2019-08-05 16:34:13 -0400

    fix line length

--- src/api/window/window.ts
+++ src/api/window/window.ts
@@ -899,7 +899,8 @@ export class _Window extends WebContents<WindowEvents> {
     }
 
     /**
-     * Joins the same window group as the specified window. Joining a group with native windows is currently not supported (method will nack).
+     * Joins the same window group as the specified window.
+     * Joining a group with native windows is currently not supported(method will nack).
      * @param { _Window | ExternalWindow } target The window whose group is to be joined
      * @return {Promise.<void>}
      * @tutorial Window.joinGroup
