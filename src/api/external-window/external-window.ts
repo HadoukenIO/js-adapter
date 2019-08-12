4094a69abf2802824fb85a92f656e2f3f3f7cc62
David H <davidhamberlin@gmail.com>
2019-08-12 10:09:08 -0400

    RUN-5480: remove undesired nwi api methods

--- src/api/external-window/external-window.ts
+++ src/api/external-window/external-window.ts
@@ -22,26 +22,6 @@ export default class ExternalWindowModule extends Base {
         await this.wire.sendAction('register-native-external-window', identity);
         return new ExternalWindow(this.wire, identity);
     }
-
-    /**
-     * Synchronously returns an external window object that represents an
-     * existing external window.
-     * This method is intended for debugging / experimentation only and should not be
-     * used in production. It will not handle errors gracefully in cases such as an attempt
-     * to wrap a non-existent window.
-     * Use `ExternalWindow.wrap` instead.
-     * @param { Identity } identity
-     * @return {ExternalWindow}
-     * @static
-     * @experimental
-     * @tutorial Window.wrapSync
-     */
-    public wrapSync(identity: Identity): ExternalWindow {
-        console.warn('ExternalWindow.wrapSync is only intended for debugging and may not handle errors properly.'
-            + '\nUse ExternalWindow.wrap instead.');
-        this.wire.sendAction('register-native-external-window', identity);
-        return new ExternalWindow(this.wire, identity);
-    }
 }
 
 /**
@@ -82,28 +62,6 @@ export class ExternalWindow extends EmitterBase<ExternalWindowEvents> {
     public async close(): Promise<void> {
         await this.wire.sendAction('close-external-window', this.identity);
         Object.setPrototypeOf(this, null);
-    }
-
-    /**
-     * Prevents a user from changing an external window's size/position
-     * when using the window's frame.
-     * @return {Promise.<void>}
-     * @experimental
-     * @tutorial Window.disableUserMovement
-     */
-    public async disableUserMovement(): Promise<void> {
-        await this.wire.sendAction('disable-external-window-user-movement', this.identity);
-    }
-
-    /**
-     * Re-enables user changes to an external window's size/position
-     * when using the window's frame.
-     * @return {Promise.<void>}
-     * @experimental
-     * @tutorial Window.enableUserMovement
-     */
-    public async enableUserMovement(): Promise<void> {
-        await this.wire.sendAction('enable-external-window-user-movement', this.identity);
     }
 
     /**
