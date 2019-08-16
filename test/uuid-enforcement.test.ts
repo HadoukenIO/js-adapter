bcd8eeeff3719fd922759a6117ccdaf8a83054d9
licui <li@openfin.co>
2019-08-16 14:33:40 -0400

    RUN-5491 Fix incorrectly passing security realm value issue

--- test/uuid-enforcement.test.ts
+++ test/uuid-enforcement.test.ts
@@ -15,7 +15,10 @@ const newManifestPath = (uuid: string) => path.resolve(`test/uuid-enforcement-ap
 const createManifest = (uuid: string, securityRealm = `uuid-enforcement-realm-${realmCount += 1}`) => {
     const manifest = {
         startup_app: { uuid, name: uuid },
-        runtime: { version: testVersion, securityRealm }
+        runtime: {
+            version: testVersion,
+            arguments: '--enable-mesh --security-realm=' + securityRealm
+        }
     };
     manifestPath = newManifestPath(uuid);
     fs.writeFileSync(manifestPath, JSON.stringify(manifest));
