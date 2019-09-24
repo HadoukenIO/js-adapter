33e07f265be09de1b0d3dfd4b536d29792407aea
Michael Coates <MichaelMCoates@gmail.com>
2019-09-24 16:57:03 -0400

    Add WebContents Events to BrowserView. Add a few events to WebContents

--- src/api/events/browserview.ts
+++ src/api/events/browserview.ts
@@ -3,9 +3,13 @@ import { WindowEvent } from './base';
 
 export interface ViewEventMapping<Topic = string, Type = string> extends WebContentsEventMapping {
     'attached': WindowEvent<Topic, Type>;
+    'crashed': WindowEvent<Topic, Type>;
     'created': WindowEvent<Topic, Type>;
+    'did-change-theme-color': WindowEvent<Topic, Type>;
     'destroyed': WindowEvent<Topic, Type>;
     'hidden': WindowEvent<Topic, Type>;
+    'page-favicon-updated': WindowEvent<Topic, Type>;
+    'page-title-updated': WindowEvent<Topic, Type>;
     'shown': WindowEvent<Topic, Type>;
 }
 
